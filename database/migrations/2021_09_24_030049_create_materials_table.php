643ee7862ffc1f8257cca0beece406da29a998ce
Eng Siang Teo <esteo.2019@scis.smu.edu.sg>
2021-09-24 03:09:03 +0000

    SPMG5T1-18 Update Jira

---
+++ database/migrations/2021_09_24_030049_create_materials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_url');
+            $table->binary('file');
+            $table->foreignId('section_id');
+            $table->foreign('section_id')->references('id')->on('sections');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+}
