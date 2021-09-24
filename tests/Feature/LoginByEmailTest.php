159a35c5a0a26ebad4b11d5d219261156af4f992
Eng Siang Teo <esteo.2019@scis.smu.edu.sg>
2021-09-24 04:48:05 +0000

    SPMG5T1-50 Login Testing

---
+++ tests/Feature/LoginByEmailTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\Api\v1\Web\Account\Auction\Login\LoginApiController;
+
+use Artisan;
+use Tests\TestCase;
+use App\Modules\Account\User\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginByEmailTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /**
+     * Test that existing LMS tokens will be revoked
+     *
+     * @return void
+     */
+    public function test_existing_lms_tokens_will_be_revoked()
+    {
+        // NOTE: need to run passport install to generate token
+        Artisan::call('passport:install');
+
+        $user = User::Create([
+            'email' => 'engsiang@onestop.com',
+            'name' => 'eng siang',
+            'password' => 'testing1234',
+            'role' => 'Learner'
+        ]);
+
+        $payload = [
+            'email' => 'engsiang@onestop.com',
+            'name' => 'eng siang',
+            'password' => 'testing1234',
+            'role' => 'Learner'
+        ];
+
+        $appName = 'LMS';
+        $token1 = $user->createToken($appName);
+        $token2 = $user->createToken($appName);
+
+        $otherAppName = 'other-app-name';
+        $token3 = $user->createToken($otherAppName);
+
+        $this
+            ->json('POST', '/api/loginByEmail', $payload)
+            ->assertOk();
+
+
+        $this
+            ->assertDatabaseHas('oauth_access_tokens', [
+                'user_id' => $user->id,
+                'id' => $token1->token->id,
+                'name' => $appName,
+                'revoked' => true
+            ])
+            ->assertDatabaseHas('oauth_access_tokens', [
+                'user_id' => $user->id,
+                'id' => $token2->token->id,
+                'name' => $appName,
+                'revoked' => true
+            ])
+            ->assertDatabaseHas('oauth_access_tokens', [
+                'user_id' => $user->id,
+                'id' => $token3->token->id,
+                'name' => $otherAppName,
+                'revoked' => false
+            ]);
+    }
+}
