20c3521dc2b9687d2a80b4459b0fd1b28c179385
Eng Siang <teo-eng-siang@hotmail.com>
2021-09-29 13:12:51 +0800

    Format Fixes

--- app/Modules/Seeder/Models/CourseClass.php
+++ app/Modules/Seeder/Models/CourseClass.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class CourseClass extends Model
 {
-	/**
+    /**
      * The table associated with the model.
      *
      * By convention, the "snake case", plural name of the class will be used as the table name unless another name is explicitly specified
      *
      * @var string
      */
-	protected $table = 'classes';
+    protected $table = 'classes';
 
     /**
      * The attributes that aren't mass assignable.
